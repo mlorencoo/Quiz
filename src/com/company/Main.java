dc57b2425f7ef5a5c3a92d56c2969ed46d4ccf44
Użytkownik <lorenc.marek94@gmail.com>
2018-02-06 15:38:39 +0100

    z 10 losowanie 5 pytan

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,90 +5,90 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-/*
-
-
-
-XASKNXLKASXKLAS
- */
-
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        readQuestionFile ();
-        System.out.println ( "odpowiedz na pytania" );
+        int counter = 0;
+        int counterCorectAnswers = 0;
         List myAnswersList = new ArrayList ();
-        List corectAnswersList = new ArrayList ();
-        myAnswersList = myAnswers ();
-        corectAnswersList = readAnswersFile ();
-        if (checkResult ( myAnswersList, corectAnswersList )) {
-            System.out.println ( "wow brawo moj ty prumsie" );
-            System.out.println ( "Twoje odpowiedzi " + myAnswersList );
-            System.out.println ( "Poprawne odpowiedzi " + corectAnswersList );
-        } else {
-            System.out.println ( "Popraw sie gamoniu" );
-            System.out.println ( "Twoje odpowiedzi " + myAnswersList );
-            System.out.println ( "Poprawne odpowiedzi " + corectAnswersList );
+        Random random = new Random ();
+        List questionList = readQuestionFile ();
+        List corectAnswersList = readAnswersFile ();
+        int numberQuestion;
+        int howManyQuestion = 5;
+        List useNumber = new ArrayList ();
+        System.out.println ( "Quiz z wiedzy o postawach automatyki" );
+        System.out.println ( "Odpowiedz na piec pytan " );
+        while (counter < howManyQuestion) {
+            numberQuestion = random.nextInt ( 10 );
+            if (!useNumber.contains ( numberQuestion )) {
+                randomQuestion ( questionList, numberQuestion );
+                System.out.println ( "odpowiedz na pytanie" );
+                myAnswersList = myAnswers ( myAnswersList );
+                if (checkResult ( myAnswersList, corectAnswersList, counter, numberQuestion )) {
+                    counterCorectAnswers++;
+                }
+                counter++;
+                useNumber.add ( numberQuestion );
+            }
+        }
+        System.out.println ( "Koniec" );
+        System.out.println ( "Uzyskales " + 100 * counterCorectAnswers / howManyQuestion + " % poprawnych odpowiedzi" );
+    }
+
+    public static void randomQuestion(List questionList, int numberQuestion) {
+
+        for (int i = numberQuestion * 5; i < numberQuestion * 5 + 5; i++) {
+            System.out.println ( questionList.get ( i ) );
         }
     }
 
-    public static boolean checkResult(List myAnswersList, List corectAnswersList) {
-        int counter=0;
+    public static boolean checkResult(List myAnswersList, List corectAnswersList, int counter, int numberQuestion) {
         boolean flag = false;
-        for (int i = 0; i < 5; i++) {
-            if (myAnswersList.get ( i ).equals ( corectAnswersList.get ( i ) )) {
-                counter++;
-            }
-        }
-        if(counter==5){
-            flag=true;
-        }
-        System.out.println ("procent poprawynych odpowiedzi "+100*counter/5+" %");
+        if (myAnswersList.get ( counter ).equals ( corectAnswersList.get ( numberQuestion ) )) {
+            System.out.println ( "poprawna odpowiedz " );
+            flag = true;
+        } else System.out.println ( "zle!!! poprawna odpowiedz to " + corectAnswersList.get ( numberQuestion ) );
         return flag;
     }
 
-    public static List<String> myAnswers() {
-        int i = 0;
-        List list = new ArrayList ();
-        while (i < 5) {
-            Scanner scanner = new Scanner ( System.in );
-            list.add ( scanner.next () );
-            i++;
-        }
-        return list;
+    public static List<String> myAnswers(List myAnswersList) {
+        Scanner scanner = new Scanner ( System.in );
+        myAnswersList.add ( scanner.next () );
+        return myAnswersList;
     }
 
     public static List<String> readAnswersFile() throws IOException {
         FileReader fileReader = new FileReader ( "C:\\Users\\Użytkownik\\IdeaProjects\\Quiz\\QuizOdp.txt" );
         BufferedReader bufferedReader = new BufferedReader ( fileReader );
-        List list = new ArrayList ();
+        List corectAnswersList = new ArrayList ();
         try {
             String textLine = bufferedReader.readLine ();
-
             do {
-                list.add ( textLine );
+                corectAnswersList.add ( textLine );
                 textLine = bufferedReader.readLine ();
             } while (textLine != null);
         } finally {
             bufferedReader.close ();
         }
-        return list;
+        return corectAnswersList;
     }
 
-    public static void readQuestionFile() throws IOException {
+    public static List readQuestionFile() throws IOException {
         FileReader fileReader = new FileReader ( "C:\\Users\\Użytkownik\\IdeaProjects\\Quiz\\QuizPytania.txt" );
         BufferedReader bufferedReader = new BufferedReader ( fileReader );
-
+        List listQuestion = new ArrayList ();
         try {
             String textLine = bufferedReader.readLine ();
             do {
-                System.out.println ( textLine );
-
+                listQuestion.add ( textLine );
                 textLine = bufferedReader.readLine ();
+
             } while (textLine != null);
         } finally {
             bufferedReader.close ();
         }
+        return listQuestion;
     }
 }
